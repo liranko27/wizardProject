a2cd58fd9c532581181353b1c55228b0e1eac0fb
baram104 <baram104@gmail.com>
2022-06-27 14:36:38 +0300

    added redirect to next page

--- js/app2.js
+++ js/app2.js
@@ -9,6 +9,12 @@ const nextBtn = document.querySelector("#next_btn");
 const streetInput = document.querySelector("#street_input");
 const numberInput = document.querySelector("#number_input");
 const inputsArray = [streetInput, numberInput, cityInput];
+const wizardDetailsObj = JSON.parse(localStorage.getItem("wizardDetailsObj"));
+if (wizardDetailsObj.phase2) {
+  cityInput.value = wizardDetailsObj.phase2.city;
+  streetInput.value = wizardDetailsObj.phase2.street;
+  numberInput.value = wizardDetailsObj.phase2.number;
+}
 const isCityInputValid = () => {
   return cityInput.value;
 };
@@ -36,4 +42,9 @@ nextBtn.addEventListener("click", () => {
   for (const input of inputsArray) {
     if (input.classList.contains("is-invalid")) return;
   }
+
+  wizardDetailsObj.phase2.city = cityInput.value;
+  wizardDetailsObj.phase2.street = streetInput.value;
+  wizardDetailsObj.phase2.number = numberInput.value;
+  location.href("../phase3.html");
 });
