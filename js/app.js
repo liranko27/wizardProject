0637dd5a4d66dc56cd3e2a88798f48989c65e0f9
baram104 <baram104@gmail.com>
2022-06-27 15:36:45 +0300

    added progressBarUpdate function to app.js

--- js/app.js
+++ js/app.js
@@ -1,2 +1,13 @@
 //Progress bar check
-//Page navigation due progress 
+const progressBar = document.querySelector(".progress-bar");
+const formFlow = JSON.parse(localStorage.getItem("formFlow"));
+const updateProgressBar = () => {
+  let barPrecentage = 0;
+  for (const page in formFlow) {
+    if (page === "welcome") continue;
+    barPrecentage += 25;
+  }
+  progressBar.classList[1] = `w-${barPrecentage}`;
+};
+
+//Page navigation due progress
