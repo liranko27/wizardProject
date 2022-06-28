01898d5672145a883453b987b4c796fc20e7d037
baram104 <baram104@gmail.com>
2022-06-28 15:06:20 +0300

    fixed styling

--- js/welcome.js
+++ js/welcome.js
@@ -1,47 +1,45 @@
-//elements 
-const aggrement = document.querySelector('#agreement')
-const agreeWrap = document.querySelector('.agree-wrap')
-const continueBtn = document.querySelector('.continue-btn')
+//elements
+const aggrement = document.querySelector("#agreement");
+const agreeWrap = document.querySelector(".agree-wrap");
+const continueBtn = document.querySelector(".continue-btn");
 
-continueBtn.addEventListener('click',()=>{
-    if(aggrement.checked){
-        console.log('Yes')
-        createFormFlowObj()
-        initWizardDetailsObj()
-        window.location.replace('phase1.html')
-    }
-    else{
-        agreeWrap.classList.add('wrong-input')
-    }
-})
+continueBtn.addEventListener("click", () => {
+  if (aggrement.checked) {
+    createFormFlowObj();
+    initWizardDetailsObj();
+    window.location.replace("phase1.html");
+  } else {
+    agreeWrap.classList.add("wrong-input");
+  }
+});
 
-function createFormFlowObj(){
-    const formFlow = {
-        welcome:true,
-        phase1:false,
-        phase2:false,
-        phase3:false,
-        phase4:false,
-    }
-    localStorage.setItem('formFlow',JSON.stringify(formFlow))
+function createFormFlowObj() {
+  const formFlow = {
+    welcome: true,
+    phase1: false,
+    phase2: false,
+    phase3: false,
+    phase4: false,
+  };
+  localStorage.setItem("formFlow", JSON.stringify(formFlow));
 }
 
-function initWizardDetailsObj(){
-    const wizardDetailsObj={
-        phase1:{
-            fullName:'',
-            email:'',
-            birthDate:'',
-        },
-        phase2:{
-            city:'',
-            street:'',
-            num:'',
-        },
-        phase3:{
-           imgSrc:'',
-           hobbies:[], 
-        },
-    }
-    localStorage.setItem('wizardDetailsObj',JSON.stringify(wizardDetailsObj))
+function initWizardDetailsObj() {
+  const wizardDetailsObj = {
+    phase1: {
+      fullName: "",
+      email: "",
+      birthDate: "",
+    },
+    phase2: {
+      city: "",
+      street: "",
+      num: "",
+    },
+    phase3: {
+      imgSrc: "",
+      hobbies: [],
+    },
+  };
+  localStorage.setItem("wizardDetailsObj", JSON.stringify(wizardDetailsObj));
 }
