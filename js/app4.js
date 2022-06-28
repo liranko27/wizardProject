517cb80759e9ad00b05e4c969e492afbfdff7fe6
baram104 <baram104@gmail.com>
2022-06-28 13:12:05 +0300

    added functionality to phase 4

---
+++ js/app4.js
@@ -0,0 +1,46 @@
+const cardBody = document.querySelector(".card-body");
+const cardImg = document.querySelector("#card_img");
+const newWizardBtn = document.querySelector("#new_wizard_btn");
+const prevBtn = document.querySelector("#previous_btn");
+
+const updateWizardDetailsSummary = () => {
+  const fullNameCardTitle = document.createElement("h5");
+  fullNameCardTitle.classList.add("card-title");
+  fullNameCardTitle.innerText = wizardDetailsObj.phase1.fullName;
+  const emailCardText = document.createElement("p");
+  emailCardText.classList.add("card-text");
+  emailCardText.innerText = wizardDetailsObj.phase1.email;
+  const birthDateText = document.createElement("p");
+  birthDateText.classList.add("card-text");
+  birthDateText.innerText = wizardDetailsObj.phase1.birthDate;
+  const addressText = document.createElement("p");
+  addressText.classList.add("card-text");
+  addressText.innerText = `City :${wizardDetailsObj.phase2.city}, Street: ${wizardDetailsObj.phase2.street}, Number: ${wizardDetailsObj.phase2.num}`;
+  const hobbiesText = document.createElement("p");
+  hobbiesText.classList.add("card-text");
+  hobbiesText.innerText = wizardDetailsObj.phase3.hobbies.join(",");
+  cardImg.src = wizardDetailsObj.phase3.imgSrc;
+  cardBody.append(
+    fullNameCardTitle,
+    emailCardText,
+    birthDateText,
+    addressText,
+    hobbiesText
+  );
+};
+updateWizardDetailsSummary();
+
+const updateFormFlowPrevBtnClicked = () => {
+  formFlow.phase3 = false;
+  localStorage.setItem("formFlow", JSON.stringify(formFlow));
+};
+
+prevBtn.addEventListener("click", () => {
+  updateFormFlowPrevBtnClicked();
+  window.location.replace("../pages/phase3.html");
+});
+
+newWizardBtn.addEventListener("click", () => {
+  localStorage.clear();
+  window.location.replace("../pages/welcome.html");
+});
